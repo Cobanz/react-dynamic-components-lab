f73e0cce4d76eb11495638ecdb3b52cf3e1c8c7c
Charles Grant Nichols <grant@inkdots.com>
2021-04-26 14:46:59 -0500

    commit tree ac8e586d31273edd7eb15aeded0a15ecc76cb3af

--- src/Comment.js
+++ src/Comment.js
@@ -1,1 +1,13 @@
 //your code here
+import React, { Componet } from 'react'
+
+
+export default class Comment extends Componet {
+    render() {
+        return (
+            <div className="comment">
+                {this.props.commentText}
+            </div>
+        )
+    }
+}
